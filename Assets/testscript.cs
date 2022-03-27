1a0ec2d00d0965d22618cf44cc41412a69ec33fb
Carlos Santamaria <titan-1719@hotmail.com>
2022-03-27 18:05:46 -0500

    Ingreso del proyecto completo
    Archivos agregados

---
+++ Assets/testscript.cs
@@ -0,0 +1,26 @@
+using System;
+using CryEngine;
+
+namespace CryEngine.Game
+{
+	[EntityComponent(Guid="6bb85296-e90d-9e99-8d7f-8bada8f904d8")]
+	public class testscript : EntityComponent
+	{
+		/// <summary>
+		/// Called at the start of the game.
+		/// </summary>
+		protected override void OnGameplayStart()
+		{
+			
+		}
+
+		/// <summary>
+		/// Called once every frame when the game is running.
+		/// </summary>
+		/// <param name="frameTime">The time difference between this and the previous frame.</param>
+		protected override void OnUpdate(float frameTime)
+		{
+
+		}
+	}
+}
